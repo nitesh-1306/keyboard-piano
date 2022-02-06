3d17ec09dd6c32d7abf5dc5d4a0c92bdc9c737fa
Nitesh-13 <niteshmali132002@gmail.com>
2022-02-06 15:45:07 +0530

    Implemented all 10 notes for playing

--- keyboardpiano.cpp
+++ keyboardpiano.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 // SDL (Simple DirectMedia Layer)
 //  - multimedia library that provides crossplatform access to audio, keyboard, mouse, etc.
@@ -10,6 +11,8 @@
 using namespace std;
 
 void print_piano();
+string get_tune_filename(int tune_number);
+void play_tune(int tune_number);
 
 int main() {
 
@@ -25,27 +28,49 @@ int main() {
         return 1;
     }
 
-    Mix_Music* music = Mix_LoadMUS("ringtone.mp3");
-    if (!music) {
-        cerr << "Failed to load MP3: " << Mix_GetError() << endl;
-        Mix_CloseAudio();
-        SDL_Quit();
-        return 1;
+    string tuneFilename;
+    int tuneNumber;
+
+    while (true) {
+        cout << "Enter a tune number from 0 to 9 to play (or -1 to quit): ";
+        cin >> tuneNumber;
+
+        if(tuneNumber == -1){
+            break;
+        }
+
+        if(tuneNumber >= 0 && tuneNumber <= 9){
+            tuneFilename = get_tune_filename(tuneNumber);
+
+            Mix_Music* music = Mix_LoadMUS(tuneFilename.c_str());
+            if (!music) {
+                cerr << "Failed to load MP3: " << Mix_GetError() << endl;
+                continue;
+            }
+
+            cout << "Playing tune " << tuneNumber << "..." << endl;
+
+            Mix_PlayMusic(music, 0);
+            while (Mix_PlayingMusic()) {
+                SDL_Delay(100);
+            }
+
+            Mix_FreeMusic(music);
+        }
+        else{
+            cout << "Invalid tune number. Please enter a number from 0 to 9." << endl;
+        }
     }
 
-    cout << "Playing MP3..." << endl;
-    Mix_PlayMusic(music, 0);
-    while (Mix_PlayingMusic()) {
-        SDL_Delay(100);
-    }
-
-    Mix_FreeMusic(music);
     Mix_CloseAudio();
     SDL_Quit();
 
     return 0;
 }
 
+string get_tune_filename(int tune_number) {
+    return "piano_tunes/" + to_string(tune_number) + ".mp3";
+}
 
 void print_piano() {
     cout << "=============================================================================\n"
